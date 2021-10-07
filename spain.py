21fda62457c90e000d008462a38450b8588a6be4
deadly-cassowary <92064834+deadly-cassowary@users.noreply.github.com>
2021-10-07 22:11:54 +1100

    Add method to plot correlation

--- spain.py
+++ spain.py
@@ -6,6 +6,7 @@ import os
 import glob
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Params
@@ -17,6 +18,7 @@ I_WANT_TO_DOWNLOAD_RAW_VACCINE_FILES = True
 I_WANT_TO_EXTRACT_TOTAL_VACCINES_RATE = True
 I_WANT_TO_DOWNLOAD_RAW_CASES_FILE = True
 I_WANT_TO_PLOT_VACCINES_VS_CASES_LINEAR = True
+I_WANT_TO_PLOT_VACCINES_VS_CASES_CORRELATION = True
 
 SPAIN_FOLDER = ""
 
@@ -83,25 +85,68 @@ def download_cases_file():
         print("Exception downloading cases file: %s" % (str(ex)))
 
 
+def get_vaccines():
+    df_vaccines = pd.read_csv(SPAIN_FOLDER + OUTPUT_VACCINATION_FILE)
+    df_vaccines["DATE_TIME"] = pd.to_datetime(df_vaccines[OUTPUT_COL_DATE], format="%Y%m%d")
+    return df_vaccines
+
+
+def get_cases():
+    df_cases = pd.read_csv(SPAIN_FOLDER + CASES_FILE)
+    df_cases["DATE_TIME"] = pd.to_datetime(df_cases[CASES_COL_DATE], format="%Y-%m-%d")
+    return df_cases
+
+
 # Uses vaccination csv file and cases file to plot linear progression
 def plot_vaccines_vs_cases_linear():
     fig, ax1 = plt.subplots()
     ax1.title.set_text('Spain Cases VS % Vaccination')
 
     # Vaccines
-    df_vaccines = pd.read_csv(SPAIN_FOLDER + OUTPUT_VACCINATION_FILE)
-    df_vaccines["DATE_TIME"] = pd.to_datetime(df_vaccines[OUTPUT_COL_DATE], format="%Y%m%d")
+    df_vaccines = get_vaccines()
     df_vaccines.plot(x="DATE_TIME", y=OUTPUT_COL_VACCINATED, color='red', ax=ax1)
 
     # Cases (file was manually downloaded)
     ax2 = ax1.twinx()
-    df_cases = pd.read_csv(SPAIN_FOLDER + CASES_FILE)
-    df_cases["DATE_TIME"] = pd.to_datetime(df_cases[CASES_COL_DATE], format="%Y-%m-%d")
+    df_cases = get_cases()
     filter_from_date = df_cases["DATE_TIME"] >= df_vaccines["DATE_TIME"][0]
     df_cases = df_cases[filter_from_date]
     df_cases.groupby(["DATE_TIME"]).sum()[CASES_COL_CASES].plot(ax=ax2, label="Cases")
 
     plt.legend(loc="upper left", bbox_to_anchor=(0, 0.9))
+    plt.show()
+
+
+# Uses vaccination csv and cases file to plot from May and from 60% vaccinated with correlation
+def plot_vaccines_vs_cases_correlation():
+    # Calculation data from May
+    vaccines_no_empty_days = get_vaccines().fillna(method='ffill')  # Dunno if there's a better way to fill the gaps
+    cases_raw = get_cases()
+    # I use only the cases for which we have a valid vaccination % (I know it's lazy and I could do better but shouldn't impact result too much)
+    filter_from_date = cases_raw["DATE_TIME"].isin(vaccines_no_empty_days["DATE_TIME"].values)
+    cases_values = cases_raw[filter_from_date].groupby(["DATE_TIME"]).sum()[CASES_COL_CASES].values
+    vaccines_values = vaccines_no_empty_days[OUTPUT_COL_VACCINATED].values
+    coef_full_data = np.corrcoef(cases_values, vaccines_values)
+
+    # Calculations for over 60% vaccination
+    over_60 = vaccines_no_empty_days[OUTPUT_COL_VACCINATED] > 0.6
+    vaccines_over_60 = vaccines_no_empty_days[over_60]
+    filter_from_date = cases_raw["DATE_TIME"].isin(vaccines_over_60["DATE_TIME"].values)
+    cases_from_60_values = cases_raw[filter_from_date].groupby(["DATE_TIME"]).sum()[CASES_COL_CASES].values
+    vaccines_from_60_values = vaccines_over_60[OUTPUT_COL_VACCINATED].values
+    coef_60 = np.corrcoef(cases_from_60_values, vaccines_from_60_values)
+
+    # Let's plot
+    fig, ax = plt.subplots(ncols=2)
+    ax[0].scatter(vaccines_values, cases_values)
+    ax[0].title.set_text('Correlation = ' + "{:.2f}".format(coef_full_data[0, 1]))
+    ax[0].set(xlabel='Vaccinated', ylabel='Cases')
+
+    ax[1].scatter(vaccines_from_60_values, cases_from_60_values)
+    ax[1].title.set_text('Correlation from 60% = ' + "{:.2f}".format(coef_60[0, 1]))
+    ax[1].set(xlabel='Vaccinated', ylabel='Cases')
+
+    fig.subplots_adjust(wspace=.4)
     plt.show()
 
 
@@ -124,5 +169,8 @@ if __name__ == "__main__":
 
     if I_WANT_TO_PLOT_VACCINES_VS_CASES_LINEAR:
         plot_vaccines_vs_cases_linear()
+
+    if I_WANT_TO_PLOT_VACCINES_VS_CASES_CORRELATION:
+        plot_vaccines_vs_cases_correlation()
 
     print("Adios")
